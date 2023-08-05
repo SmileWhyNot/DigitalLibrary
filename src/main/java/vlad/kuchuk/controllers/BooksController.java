c9d7cbac47af19ce555c7e726c3ea2f13d2bb1ba
Vlad Kuchuk <bboysmile2003@mail.ru>
2023-08-05 15:09:19 +0300

    Made second part of rewriting JdbcTeplate => Spring Data JPA + Hibernate changes made for book part deleted database.properties.origin deleted DAO classes, now they are useless

--- src/main/java/vlad/kuchuk/controllers/BooksController.java
+++ src/main/java/vlad/kuchuk/controllers/BooksController.java
@@ -8,84 +8,87 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import vlad.kuchuk.models.Book;
 import vlad.kuchuk.models.Person;
+import vlad.kuchuk.services.BooksService;
+import vlad.kuchuk.services.PeopleService;
 
-//@Controller
-//@RequestMapping("/books")
-//public class BooksController {
-//    private final BookDAO bookDAO;
-//    private final PersonDAO personDAO;
-//
-//    @Autowired
-//    public BooksController(BookDAO bookDAO, PersonDAO personDAO) {
-//        this.bookDAO = bookDAO;
-//        this.personDAO = personDAO;
-//    }
-//
-//    @GetMapping()
-//    public String index(Model model) {
-//        model.addAttribute("books", bookDAO.index());
-//        return "books/index";
-//    }
-//
-//    @GetMapping("/{id}")
-//    public String showBook(@PathVariable("id") int id,
-//                           Model model,
-//                           @ModelAttribute("person") Person person) {
-//        model.addAttribute("book", bookDAO.show(id));
-//        model.addAttribute("bookReader", bookDAO.getReader(id));
-//        model.addAttribute("people", personDAO.index());
-//        return "books/showBook";
-//    }
-//
-//    @PatchMapping("/{id}/updateReader")
-//    public String updateReader(@PathVariable("id") int id,
-//                               @ModelAttribute("person") Person person) {
-//        if (bookDAO.getReader(id) != null) {
-//            bookDAO.removeBookReader(id);
-//        } else if (bookDAO.getReader(id) == null) {
-//            bookDAO.setBookReader(id, person);
-//        }
-//        return "redirect:/books/{id}";
-//    }
-//
-//    @GetMapping("/newBook")
-//    public String newBook(@ModelAttribute("book") Book book) {
-//        return "books/newBook";
-//    }
-//
-//    @PostMapping
-//    public String createBook(@ModelAttribute("book") @Valid Book book,
-//                             BindingResult bindingResult) {
-//
-//        if (bindingResult.hasErrors())
-//            return "/books/newBook";
-//
-//        bookDAO.create(book);
-//        return "redirect:/books";
-//    }
-//
-//    @GetMapping("/{id}/editBook")
-//    public String editBook(@PathVariable("id") int id,
-//                           Model model) {
-//        model.addAttribute("book", bookDAO.show(id));
-//        return "/books/editBook";
-//    }
-//
-//    @PatchMapping("/{id}")
-//    public String update(@PathVariable("id") int id,
-//                         @ModelAttribute("book") Book book,
-//                         BindingResult bindingResult) {
-//        if (bindingResult.hasErrors())
-//            return "/books/editBook";
-//
-//        bookDAO.update(id, book);
-//        return "redirect:/books";
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public String delete(@PathVariable("id") int id) {
-//        bookDAO.delete(id);
-//        return "redirect:/books";
-//    }
-//
-//}
+@Controller
+@RequestMapping("/books")
+public class BooksController {
+
+    private final BooksService booksService;
+    private final PeopleService peopleService;
+
+    @Autowired
+    public BooksController(BooksService booksService, PeopleService peopleService) {
+        this.booksService = booksService;
+        this.peopleService = peopleService;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("books", booksService.findAll());
+        return "books/index";
+    }
+
+    @GetMapping("/{id}")
+    public String showBook(@PathVariable("id") int id,
+                           Model model,
+                           @ModelAttribute("person") Person person) {
+        model.addAttribute("book", booksService.findOne(id));
+        model.addAttribute("bookReader", booksService.findReader(id));
+        model.addAttribute("people", peopleService.findAll());
+        return "books/showBook";
+    }
+
+    @PatchMapping("/{id}/updateReader")
+    public String updateReader(@PathVariable("id") int id,
+                               @ModelAttribute("person") Person person) {
+        if (booksService.findReader(id) != null) {
+            booksService.removeBookReader(id);
+        } else if (booksService.findReader(id) == null) {
+            booksService.setBookReader(id, person);
+        }
+        return "redirect:/books/{id}";
+    }
+
+    @GetMapping("/newBook")
+    public String newBook(@ModelAttribute("book") Book book) {
+        return "books/newBook";
+    }
+
+    @PostMapping
+    public String createBook(@ModelAttribute("book") @Valid Book book,
+                             BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors())
+            return "/books/newBook";
+
+        booksService.save(book);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/{id}/editBook")
+    public String editBook(@PathVariable("id") int id,
+                           Model model) {
+        model.addAttribute("book", booksService.findOne(id));
+        return "/books/editBook";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@PathVariable("id") int id,
+                         @ModelAttribute("book") Book book,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "/books/editBook";
+
+        booksService.update(book, id);
+        return "redirect:/books";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        booksService.delete(id);
+        return "redirect:/books";
+    }
+
+}
--- src/main/java/vlad/kuchuk/services/BooksService.java
+++ src/main/java/vlad/kuchuk/services/BooksService.java
@@ -8,6 +8,7 @@ import vlad.kuchuk.models.Person;
 import vlad.kuchuk.repositories.BooksRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -19,7 +20,49 @@ public class BooksService {
         this.booksRepository = booksRepository;
     }
 
+    public List<Book> findAll() {
+        return booksRepository.findAll();
+    }
+
+    public Book findOne(int id) {
+        Optional<Book> book = booksRepository.findById(id);
+        return book.orElse(null);
+    }
+
     public List<Book> findBooksByReader(Person reader) {
         return booksRepository.findByReader(reader);
+    }
+
+    public Person findReader(int id){
+        Optional<Book> book = booksRepository.findById(id);
+        return book.map(Book::getReader).orElse(null);
+    }
+
+    @Transactional
+    public void removeBookReader(int id) {
+        Optional<Book> book = booksRepository.findById(id);
+        book.ifPresent(value -> value.setReader(null));
+    }
+
+    @Transactional
+    public void setBookReader(int id, Person person) {
+        Optional<Book> book = booksRepository.findById(id);
+        book.ifPresent(value -> value.setReader(person));
+    }
+
+    @Transactional
+    public void save(Book book) {
+        booksRepository.save(book);
+    }
+
+    @Transactional
+    public void update(Book updatedBook, int id) {
+        updatedBook.setBookId(id);
+        booksRepository.save(updatedBook);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        booksRepository.deleteById(id);
     }
 }
