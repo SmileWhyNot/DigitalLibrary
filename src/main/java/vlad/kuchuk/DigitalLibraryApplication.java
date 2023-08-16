9d54a55c3938048bb3bf565545b1a20cc27635bb
Vlad Kuchuk <bboysmile2003@mail.ru>
2023-08-16 20:35:15 +0300

    Moved project to Spring Boot version delete configuration classes add application.properties moved templates to resources folder add DigitalLibraryApplication class update pom file with boot starters

---
+++ src/main/java/vlad/kuchuk/DigitalLibraryApplication.java
@@ -0,0 +1,13 @@
+package vlad.kuchuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalLibraryApplication.class, args);
+	}
+
+}
